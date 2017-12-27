b5d71f477557206f39566564ad4ee8ee4355eac0
Gavi Stein <gavriella.stein@gmail.com>
2017-12-27 16:28:14 +0200

    Comments in client.h

--- ex5/src/client/include/Client.h
+++ ex5/src/client/include/Client.h
@@ -10,10 +10,52 @@
 
 class Client {
 public:
+	/********************************************************************
+	 *Function name: Client()
+	 *The input: None
+	 *The output: Client object
+	 *The function operation: Constructor
+	 *********************************************************************/
 	Client();
+	
+	/********************************************************************
+	 *Function name: Client()
+	 *The input: const char* serverIP (a string representing server info), int serverPort number
+	 *The output: Client object
+	 *The function operation: Constructor
+	 *********************************************************************/
 	Client(const char *serverIP, int serverPort);
+	
+	/********************************************************************
+	 *Function name: connectToServer()
+	 *The input: None
+	 *The output: None
+	 *The function operation: Establishes a connection between the remote server and the local client
+	 *********************************************************************/
 	void connectToServer();
+	
+	/********************************************************************
+	 *Function name: sendMove()
+	 *The input: A pair of ints representing row and column coordinates
+	 *The output: None
+	 *The function operation: Sends the ints one at a time to the Server
+	 *********************************************************************/
 	void sendMove(pair<int,int> move);
+	
+	/********************************************************************
+	 *Function name: receiveOrder()
+	 *The input: None
+	 *The output: An int representing the player's order in the game
+	 *The function operation: Receives an int from the Server
+	 *********************************************************************/
+	int receiveOrder();
+	
+	/********************************************************************
+	 *Function name: receiveOrder()
+	 *The input: None
+	 *The output: A pair of ints representing row and column coordinates.
+	 *The function operation: Receives from the Server two ints and returns their pair
+	 *********************************************************************/
 	pair<int,int> receiveMove();
 	void init(const char* file);
 	void startGame(string name);
